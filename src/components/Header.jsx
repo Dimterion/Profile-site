d178622121b09a475ee63e64cec4a13b1c0d7c34
Dmitrii <dimterion59@gmail.com>
2023-05-02 19:59:59 +0200

    add header and section for the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,22 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/about">About</Link>
-    </div>
+    <header className="flex justify-between 	items-baseline m-1 lg:m-5 py-3 font-semibold bg-primary">
+      <Link className="text-lg hover:opacity-80 hover:underline" to="/">
+        @Dimterion
+      </Link>
+      <nav>
+        <Link className="mx-1 hover:opacity-80 hover:underline" to="/blog">
+          Blog
+        </Link>
+        <Link className="mx-1 hover:opacity-80 hover:underline" to="/projects">
+          Projects
+        </Link>
+        <Link className="mx-1 hover:opacity-80 hover:underline" to="/about">
+          About
+        </Link>
+      </nav>
+    </header>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1 className="text-3xl font-bold underline">Profile Site</h1>;
+  return (
+    <section className="mx-1 lg:mx-5 my-10 text-center">
+      <h1 className="text-3xl font-bold">Hey! I{"'"}m Dmitrii</h1>
+      <p className="my-10 text-2xl">
+        I code, I learn and I do stuff. Then I re-code, re-learn and re-do
+        stuff. And then I repeat it all until it works.
+      </p>
+      <Link className="hover:opacity-80 hover:underline" to="/blog">
+        Check out my blog
+      </Link>
+    </section>
+  );
 }
 
 export default Home;
