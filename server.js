33527eb87b71623c6a2fedb940fa2aa5422f3fe9
Dmitrii <dimterion59@gmail.com>
2023-05-05 11:01:45 +0200

    adjust server, add posts data

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ createServer({
       title: "Post 1",
       date: "01.05.23",
       text: "This is a text-1 example.",
-      imageUrl: "./src/assets/images/placeholderImg.jpg",
+      imageUrl: "../src/assets/images/placeholderImg.jpg",
       type: "thoughts",
     });
     server.create("post", {
@@ -19,7 +19,7 @@ createServer({
       title: "Post 2",
       date: "02.05.23",
       text: "This is a text-2 example.",
-      imageUrl: "./src/assets/images/placeholderImg.jpg",
+      imageUrl: "../src/assets/images/placeholderImg.jpg",
       type: "coding",
     });
     server.create("post", {
@@ -27,7 +27,7 @@ createServer({
       title: "Post 3",
       date: "03.05.23",
       text: "This is a text-3 example.",
-      imageUrl: "./src/assets/images/placeholderImg.jpg",
+      imageUrl: "../src/assets/images/placeholderImg.jpg",
       type: "life",
     });
     server.create("post", {
@@ -35,7 +35,7 @@ createServer({
       title: "Post 4",
       date: "04.05.23",
       text: "This is a text-4 example.",
-      imageUrl: "./src/assets/images/placeholderImg.jpg",
+      imageUrl: "../src/assets/images/placeholderImg.jpg",
       type: "thoughts",
     });
     server.create("post", {
@@ -43,7 +43,7 @@ createServer({
       title: "Post 5",
       date: "05.05.23",
       text: "This is a text-5 example.",
-      imageUrl: "./src/assets/images/placeholderImg.jpg",
+      imageUrl: "../src/assets/images/placeholderImg.jpg",
       type: "coding",
     });
   },
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Post() {
-  return <div>Post</div>;
+  const params = useParams();
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/posts/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data.posts));
+  }, [params.id]);
+
+  return (
+    <div>
+      {post ? (
+        <article className="lg:w-2/5 sm:w-3/5 mt-8 mb-16 mx-8">
+          <h2>{post.title}</h2>
+          <img src={post.imageUrl} alt={post.title} />
+          <p>{post.text}</p>
+          <small>{post.date}</small>
+          <pre>{post.type}</pre>
+        </article>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
 
 export default Post;
