eaf7eb15ac548fff3d379720b5dabc0659579fee
Dmitrii <dimterion59@gmail.com>
2023-05-07 10:45:27 +0200

    add layout to rearrange header & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Post from "./pages/Post";
+import Layout from "./components/Layout";
 import "../server";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog/:id" element={<Post />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog/:id" element={<Post />} />
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
