8149d3e72b2be224bc26e3f9befa0e3689261e52
Dmitrii <dimterion59@gmail.com>
2023-05-27 10:34:10 +0200

    start firebase setup

--- src/api.js
+++ src/api.js
@@ -1,3 +1,27 @@
+// Firebase setup
+
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.DB_API,
+  authDomain: import.meta.env.DB_DOMAIN,
+  projectId: import.meta.env.DB_PROJECTID,
+  storageBucket: import.meta.env.DB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.DB_SENDERID,
+  appId: import.meta.env.DB_APID,
+  measurementId: import.meta.env.DB_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const db = getFireStore(app);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+
+// MirageJS code below
+
 export async function getPosts(id) {
   const url = id ? `/api/posts/${id}` : "/api/posts";
   const res = await fetch(url);
