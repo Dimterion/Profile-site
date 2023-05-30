06bcfc9f40ba83a8fbc42c20dafd181d69dedead
Dmitrii <dimterion59@gmail.com>
2023-05-30 10:13:54 +0200

    add query & where to api.js

--- src/api.js
+++ src/api.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
+  query,
+  where,
   doc,
   getDoc,
   getDocs,
@@ -20,10 +22,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
 const db = getFirestore(app);
 // const analytics = getAnalytics(app);
-// eslint-disable-next-line no-unused-vars
 const postsCollectionRef = collection(db, "posts");
 
 export async function getPosts() {
@@ -44,6 +44,16 @@ export async function getPost(id) {
   };
 }
 
+export async function getProfilePosts() {
+  const q = query(postsCollectionRef, where("profileId", "==", "123"));
+  const querySnapshot = await getDocs(q);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return dataArr;
+}
+
 // MirageJS setup
 // export async function getPosts(id) {
 //   const url = id ? `/api/posts/${id}` : "/api/posts";
@@ -62,22 +72,22 @@ export async function getPost(id) {
 //   return data.posts;
 // }
 
-export async function getProfilePosts(id) {
-  const url = id ? `/api/profile/posts/${id}` : "/api/profile/posts";
-  const res = await fetch(url);
+// export async function getProfilePosts(id) {
+//   const url = id ? `/api/profile/posts/${id}` : "/api/profile/posts";
+//   const res = await fetch(url);
 
-  if (!res.ok) {
-    throw {
-      message: "Couldn't fetch posts",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
+//   if (!res.ok) {
+//     throw {
+//       message: "Couldn't fetch posts",
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  return data.posts;
-}
+//   return data.posts;
+// }
 
 export async function loginUser(creds) {
   const res = await fetch("/api/login", {
